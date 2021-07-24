170b2eb6f30938c30e10fd62f128db7431ad6fed
jeffry zaman <jeffry.zaman@gmail.com>
2021-07-24 14:35:14 -0400

    able read form and write(after removeing key) to a json file

--- util/load_json_file.py
+++ util/load_json_file.py
@@ -1,1 +1,27 @@
 import json
+
+# json.load(takes a file): in order load json file into a python object we use json.load.
+# whereas, json.loads methods takes a string of json
+
+# to load the json into a python object we need to open the file with the with open(fileName.json) statement
+
+with open('../json_files/states.json') as f:
+    data = json.load(f)
+
+print(f'type of states : {type(data)}')
+# print(states)
+
+ga = {}
+states = data['states']
+# for state in data['states'] or
+for state in states:
+    if 'Georgia' in state['name']:
+        ga = state
+        break
+
+print(f'----------| Found GA | ----------')
+print(f'Name: {ga["name"]}')
+print(f'Abbreviation: {ga["abbreviation"]}')
+print(f'Area Codes: {ga["area_codes"]}')
+for area_code in ga["area_codes"]:
+    print(area_code)
--- util/write_json_to_file.py
+++ util/write_json_to_file.py
@@ -1,1 +1,22 @@
 import json
+
+# 1. in this section we will load json from json file
+# 2. remove the area codes
+# 3. then write the modified json to a new json file
+
+
+# 1. load the states.json file in to a python object
+with open('../json_files/states.json') as f:
+    data = json.load(f)
+
+# 2. remove area code from the states:
+for state in data['states']:
+    del state['area_codes']  # this will remove the area_codes from the py data object
+
+# data_str = json.dumps(data, indent=2)
+# print(data_str)
+
+# 3. write to a new file : modified_states.json. py will create the file if it does not exist
+# dumping the python data object into the file object f
+with open(file='../json_files/modified_state.json', mode='w')as f:
+    json.dump(data, f)
